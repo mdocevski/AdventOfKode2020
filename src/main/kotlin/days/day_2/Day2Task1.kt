fb92b0367ca5b31ef03060cd000863e858d6ae5f
Marko Docevski <mdocevski@gmail.com>
2020-12-08 22:03:33 +0100

    Solve Day 2 Task 1

---
+++ src/main/kotlin/days/day_2/Day2Task1.kt
@@ -0,0 +1,17 @@
+package days.day_2
+
+class Day2Task1 {
+    fun solution(): Int {
+        val inputs = Day2Inputs.inputs
+        return inputs.count { passwordInput ->
+            val characterRepetitionsInPassword =
+                passwordInput.password.toCharArray().fold(mutableMapOf<Char, Int>()) { countMap, character ->
+                    countMap.compute(character) { _, oldValue ->
+                        (oldValue ?: 0) + 1
+                    }
+                    countMap
+                }
+            characterRepetitionsInPassword[passwordInput.characterToBeRepeated] in passwordInput.characterRepeatRange
+        }
+    }
+}
